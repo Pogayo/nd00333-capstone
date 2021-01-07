f633a2114cc21ad56ac1fc71271aeb7f1c6ab9fe
Perez Ogayo <pogayo17@alustudent.com>
2021-01-07 06:59:38 +0200

    finished train.py

---
+++ train.py
@@ -0,0 +1,92 @@
+
+import pandas as pd
+import numpy as np
+
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import roc_auc_score, accuracy_score
+from sklearn import naive_bayes
+
+
+import argparse
+import os
+import joblib
+
+from azureml.core.run import Run
+from azureml.data.dataset_factory import TabularDatasetFactory
+
+from xgboost import XGBClassifier
+
+
+def clean_data(data):
+   
+    # Clean and encode data
+    train_df = data.to_pandas_dataframe().dropna()
+
+    train_df["label"]=train_df["label"].apply(int)
+
+
+    train_df.describe()
+
+    train_df['word_count'] =train_df["text"].apply(lambda x: len(str(x).split(" ")))
+    train_df['char_count'] = train_df["text"].apply(lambda x: sum(len(word) for word in str(x).split(" ")))
+    train_df['sentence_count'] = train_df["text"].apply(lambda x: len(str(x).split(".")))
+    train_df['avg_word_length'] = train_df['char_count'] / train_df['word_count']
+    train_df['avg_sentence_lenght'] =train_df['word_count'] / train_df['sentence_count']
+    train_df.head()
+
+    train_df["label"]=train_df["label"].apply(lambda x:x+1)
+
+    vectorizer=TfidfVectorizer(strip_accents='ascii', sublinear_tf=True, min_df=2, max_df=0.5)
+
+    y=train_df.label
+
+    train_df.drop("ID", axis=1, inplace=True)
+    
+
+    X_train, X_val, y_train, y_val = train_test_split(train_df, y, test_size=0.20, random_state=42, stratify=y)
+
+    vectorizer.fit(train_df.text)
+    sparse_X_train=vectorizer.transform(X_train.text)
+    sparse_X_val=vectorizer.transform(X_val.text)
+
+
+    return  sparse_X_train, sparse_X_val, y_train, y_val
+
+
+
+
+run = Run.get_context()
+
+   
+def main():
+    # Add arguments to script
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_path', type=str, default="", help="Path of data in storage")
+    parser.add_argument('--n_estimators', type=int, default=100, help="Inverse of regularization strength. Smaller values cause stronger regularization")
+    parser.add_argument('--max_depth', type=int, default=6, help="Maximum number of iterations to converge")
+
+    args = parser.parse_args()
+    
+    run.log("Number of estimators:", np.float(args.n_estimators))
+    run.log("Max depth:", np.int(args.max_depth))
+    
+    # TODO: Create TabularDataset using TabularDatasetFactory
+
+    # data_path= "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"  #change path
+    data_path= args.data_path
+    
+    ds =TabularDatasetFactory.from_delimited_files(path=data_path, separator=",")
+
+    X_train, X_test, y_train, y_test= clean_data(ds)
+
+
+
+    model = XGBClassifier(n_estimators=args.n_estimators, max_depth=args.max_depth).fit(X_train, y_train)
+    
+    y_pred = model.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    run.log("Accuracy", np.float(accuracy))
+
+if __name__ == '__main__':
+    main()
